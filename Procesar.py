8f4eb5560626aa1a4573bde69e3464ed1282544b
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-03-07 11:16:53 -0600

    Metodos auxiliares creados

--- Procesar.py
+++ Procesar.py
@@ -13,13 +13,14 @@ class dato:
         return String
 
 class lsreducir:
-    def __init__(self, id, f1, f2):
+    def __init__(self, id,g, f1, f2):
         self.id = id
+        self.g = g
         self.f1 = f1
         self.f2 = f2
     
     def __str__(self):
-        String = str("id: ") + str(self.id)+ str("\nf1: ") + str(self.f1) + str("\nf2: ") + str(self.f2)+  str("\n")
+        String = str("id: ") + str(self.id)+ str("\nf1: ")+ str("\ngrupo: ") + str(self.g) + str(self.f1) + str("\nf2: ") + str(self.f2)+  str("\n")
         return String
 
 class frecuencias:
@@ -38,6 +39,7 @@ class Procesar:
     lista=None
     listaf=None
     listagrupos=None
+    listaPrevSuma=None
 
     def __init__(self, datos, cabeceras):
         self.datos=datos
@@ -45,9 +47,11 @@ class Procesar:
         global lista
         global listaf
         global listagrupos
+        global listaPrevSuma
         lista=linked_list()
         listaf=linked_list()
         listagrupos=linked_list()
+        listaPrevSuma=linked_list()
         
 
     def imprimir(self):
@@ -68,7 +72,7 @@ class Procesar:
         #print(lista.searchxyz(2, 3 ,2))
         ids=len(self.cabeceras)
         for id in range(int(ids)):
-            
+            gg=1
             n=self.cabeceras.search(id).dato.n
             m=self.cabeceras.search(id).dato.m
             aux=int(n)+1
@@ -95,44 +99,31 @@ class Procesar:
                     if cont==int(m):
                         ccn+=1
                         '''
-                        string = str("se encontro coincidencias en la matriz ")+str(id)+str(" filas ")+str(i)+str(" con ")+str(j+i)
+                        string = str("se encontro coincidencias en la matriz ")+str(id)+str(" grupo ")+str(gg)+str(" filas ")+str(i)+str(" con ")+str(j+i)
                         print(string)
                         '''
 
                         if agregarMismo==True:
-                            ags=lsreducir(id, i,i)
+                            ags=lsreducir(id,gg, i,i)
                             listaf.append(ags)
                             agregarMismo=False
                     
-                        aux1=lsreducir(id, i,(j+i))
+                        aux1=lsreducir(id,gg, i,(j+i))
                         listaf.append(aux1)
                 if ccn==0:
                     '''
-                    string = str("NO se encontro coincidencias en la matriz ")+str(id)+str(" fila ")+str(i)
+                    string = str("NO se encontro coincidencias en la matriz ")+str(id)+str(" grupo ")+str(gg)+str(" fila ")+str(i)
                     print(string)
                     '''
-                    ags=lsreducir(id, i,i)
+                    ags=lsreducir(id,gg, i,i)
                     listaf.append(ags)
                 elif ccn>0:
                     ccn=0
+                gg+=1
         #self.sumar()
         self.gruposFrecuencias()
         #print(listaf)
-        
-
-    def sumar(self):
-        idm=len(self.cabeceras)
-        for idmatriz in range(idm):
-            for ad in listaf.iterar():
-                if ad.dato.id==idmatriz:
-                    
-                    
-                    n=self.cabeceras.search(idmatriz).dato.n
-                    self.frecuencia(idmatriz, n, 3)
-                    #print(self.datos.searchxy(idmatriz,ad.dato.f2,1))
-
-        #print(self.datos.searchxy(0,1,1))
-        #print(listaf)
+    '''    
     def frecuencia(self, id, n, f):
         c=0
         for i in listaf.iterar():
@@ -141,7 +132,7 @@ class Procesar:
                 print(i)
         st=str(" en ")+str(f)+str(" se repite ")+str(c)    
         print(st)
-
+    '''
     def gruposFrecuencias(self):
         idm=len(self.cabeceras)
         for idmatriz in range(idm):
@@ -158,5 +149,32 @@ class Procesar:
                     aux=frecuencias(idmatriz,cont,i,c)
                     listagrupos.append(aux)
                     cont+=1
-        print(listagrupos)        
+        print(listagrupos) 
+        print(self.obtenerNgrupos(0))  
+        print(self.obtenerFrecuenciaGrupo(0,3))     
                     
+    def sumar(self):
+        idm=len(self.cabeceras)
+        for idmatriz in range(idm):
+            n=self.cabeceras.search(idmatriz).dato.n
+            m=self.cabeceras.search(idmatriz).dato.m
+            for i in listaf.iterar():
+                if i.dato.id==idmatriz:
+
+                    
+                    #print(self.datos.searchxy(idmatriz,ad.dato.f2,1))
+
+        #print(self.datos.searchxy(0,1,1))
+        #print(listaf)
+    
+    def obtenerNgrupos(self, id):
+        cont=0
+        for i in listagrupos.iterar():
+            if i.dato.id==id:
+                cont+=1
+        return cont
+
+    def obtenerFrecuenciaGrupo(self, id, grupo):
+        for i in listagrupos.iterar():
+            if i.dato.id==id and i.dato.g==grupo:
+                return int(i.dato.frecuencia)-1
